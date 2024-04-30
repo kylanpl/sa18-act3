ec7e4cdc76fc981829f7ed3a67897961adf11ba6
Kylan Plunk <kaplunk1@memphis.edu>
2024-04-30 04:32:55 -0500

    need to define max digits even though not specified

--- myapp/models.py
+++ myapp/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(max_digits=100, decimal_places=2)
     description = models.TextField()
